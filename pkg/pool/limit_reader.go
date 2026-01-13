1b59e4216b6bf0d7cb3d1d17c6e64eb8fbcf2b57
Eric Lam <tsukiko852@gmail.com>
2026-01-13 05:35:51 +0000

    feat: pass context to LimitReader for rate limiting

--- pkg/pool/limit_reader.go
+++ pkg/pool/limit_reader.go
@@ -10,19 +10,21 @@ import (
 type LimitReader struct {
 	r io.Reader
 	l *rate.Limiter
+	c context.Context
 }
 
-func NewLimitReader(r io.Reader, limit, burst int) *LimitReader {
+func NewLimitReader(ctx context.Context, r io.Reader, limit, burst int) *LimitReader {
 	return &LimitReader{
 		r: r,
 		l: rate.NewLimiter(rate.Limit(limit), burst),
+		c: ctx,
 	}
 }
 
 func (lr *LimitReader) Read(p []byte) (n int, err error) {
 	n, err = lr.r.Read(p)
 	if n > 0 {
-		lr.l.WaitN(context.Background(), n)
+		lr.l.WaitN(lr.c, n)
 	}
 	return n, err
 }
